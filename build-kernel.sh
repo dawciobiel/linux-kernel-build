e1ce20dcd4562b2863aa849787948001a0402439
dawciobiel <dawciobiel@gmail.com>
2025-09-22 04:41:43 +0200

    refactored workflow for docker

--- build-kernel.sh
+++ build-kernel.sh
@@ -9,15 +9,16 @@ if [ -z "$CONFIG_PATH" ]; then
   exit 1
 fi
 
+# Kernel version
 KERNEL_VERSION="6.16.7"
-KERNEL_SRC_DIR="/usr/src/linux-${KERNEL_VERSION}-1"
-BUILD_OBJ_DIR="/usr/src/linux-${KERNEL_VERSION}-1-obj"
+KERNEL_SRC_DIR="/usr/src/linux-${KERNEL_VERSION}"
+BUILD_OBJ_DIR="/usr/src/linux-${KERNEL_VERSION}-obj"
 RPMBUILD_DIR="/usr/src/packages"
 
 echo ">>> Installing build dependencies..."
 zypper -n ref
 zypper -n in bc bison flex gcc git make ncurses-devel perl \
-  rpm-build wget libopenssl-devel libelf-devel dwarves
+  rpm-build wget libopenssl-devel libelf-devel dwarves xz
 
 echo ">>> Downloading kernel sources..."
 cd /usr/src
@@ -31,6 +32,7 @@ echo ">>> Preparing build directories..."
 mkdir -p "$BUILD_OBJ_DIR/x86_64/default"
 mkdir -p "$RPMBUILD_DIR/BUILD" "$RPMBUILD_DIR/RPMS" "$RPMBUILD_DIR/SOURCES" "$RPMBUILD_DIR/SPECS" "$RPMBUILD_DIR/SRPMS"
 
+# Copy custom config to build object directory
 cp "$CONFIG_PATH" "$BUILD_OBJ_DIR/x86_64/default/.config"
 
 echo ">>> Running olddefconfig..."
